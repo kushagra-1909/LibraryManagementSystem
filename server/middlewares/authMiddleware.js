d17190aa547fe3a6f0317be8380b4de750f46691
kushagra2003topper <104319946+kushagra2003topper@users.noreply.github.com>
2023-12-07 12:07:19 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,62 +1,30 @@
-// const jwt = require("jsonwebtoken");
-// module.exports = function (req, res, next) {
-//   try {
-//     let token;
-//     let decoded;
-//     if(req.headers!==undefined){
-//       if(req.headers.authorization.startsWith("Bearer "){
-//       token = req.headers.authorization.split(" ")[1];
-//      decoded = jwt.verify(token, process.env.jwt_secret);
-//       }
-//     }
-    
-//     if (decoded.userId) {
-//       req.body.userIdFromToken = decoded.userId;
-//       next();
-//     } else {
-//       return res.send({
-//         success: false,
-//         message: "Invalid token",
-//       });
-//     }
-//   } catch (error) {
-//     return res.send({
-
 const jwt = require("jsonwebtoken");
-
 module.exports = function (req, res, next) {
   try {
-    const authorizationHeader = req.headers.authorization;
-
-    if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
-      return res.status(401).json({
-        success: false,
-        message: "Authorization header missing or invalid",
-      });
+    let token;
+    let decoded;
+    if(req.headers!==undefined){
+      if(req.headers.authorization.startsWith("Bearer "){
+      token = req.headers.authorization.split(" ")[1];
+     decoded = jwt.verify(token, process.env.jwt_secret);
+      }
     }
-
-    const token = authorizationHeader.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.jwt_secret);
-
+    
     if (decoded.userId) {
       req.body.userIdFromToken = decoded.userId;
       next();
     } else {
-      return res.status(401).json({
+      return res.send({
         success: false,
         message: "Invalid token",
       });
     }
   } catch (error) {
-    return res.status(500).json({
+    return res.send({
+
+
       success: false,
       message: error.message,
     });
   }
 };
-
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
