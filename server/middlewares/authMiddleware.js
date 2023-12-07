ccb3623fe1415eeb48bb2b049eaddc4772f16274
kushagra2003topper <104319946+kushagra2003topper@users.noreply.github.com>
2023-12-07 11:37:36 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -4,9 +4,10 @@ module.exports = function (req, res, next) {
     let token;
     let decoded;
     if(req.headers!==undefined){
-      if(req.headers.authorization.startsWith("Bearer ")
+      if(req.headers.authorization.startsWith("Bearer "){
       token = req.headers.authorization.split(" ")[1];
      decoded = jwt.verify(token, process.env.jwt_secret);
+      }
     }
     
     if (decoded.userId) {
