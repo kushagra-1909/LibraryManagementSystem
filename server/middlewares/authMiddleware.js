5a39e0db9cfb11b96b4bfc4a9ed066c478fae6ab
kushagra2003topper <104319946+kushagra2003topper@users.noreply.github.com>
2023-12-07 12:19:30 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,17 +1,57 @@
+// const jwt = require("jsonwebtoken");
+
+// module.exports = function (req, res, next) {
+//   try {
+//     const authorizationHeader = req.headers.authorization;
+
+//     if (!authorizationHeader || !authorizationHeader.includes("Bearer ")) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Authorization header missing or invalid",
+//       });
+//     }
+
+//     const token = authorizationHeader.split("Bearer ")[1];
+
+//     if (!token) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Token not found in authorization header",
+//       });
+//     }
+
+//     const decoded = jwt.verify(token, process.env.jwt_secret);
+
+//     if (decoded.userId) {
+//       req.body.userIdFromToken = decoded.userId;
+//       next();
+//     } else {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Invalid token",
+//       });
+//     }
+//   } catch (error) {
+//     return res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   try {
     const authorizationHeader = req.headers.authorization;
 
-    if (!authorizationHeader || !authorizationHeader.includes("Bearer ")) {
+    if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
       return res.status(401).json({
         success: false,
         message: "Authorization header missing or invalid",
       });
     }
 
-    const token = authorizationHeader.split("Bearer ")[1];
+    const token = authorizationHeader.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({
@@ -20,17 +60,24 @@ module.exports = function (req, res, next) {
       });
     }
 
-    const decoded = jwt.verify(token, process.env.jwt_secret);
+    jwt.verify(token, process.env.jwt_secret, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          success: false,
+          message: "Invalid token",
+        });
+      }
 
-    if (decoded.userId) {
-      req.body.userIdFromToken = decoded.userId;
-      next();
-    } else {
-      return res.status(401).json({
-        success: false,
-        message: "Invalid token",
-      });
-    }
+      if (decoded.userId) {
+        req.body.userIdFromToken = decoded.userId;
+        next();
+      } else {
+        return res.status(401).json({
+          success: false,
+          message: "Invalid token",
+        });
+      }
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
