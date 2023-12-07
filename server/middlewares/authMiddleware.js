74e23f83112d70854f21761c1ddf80eb717c5df7
kushagra2003topper <104319946+kushagra2003topper@users.noreply.github.com>
2023-12-07 10:49:50 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   try {
+    if(req.headers.authorization!==undefined)
     const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.jwt_secret);
     if (decoded.userId) {
