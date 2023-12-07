1d42c460c37805feb44db159bac2f76b77299116
kushagra2003topper <104319946+kushagra2003topper@users.noreply.github.com>
2023-12-07 12:13:17 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,19 +1,38 @@
 const jwt = require("jsonwebtoken");
+
 module.exports = function (req, res, next) {
   try {
-    const token = req.headers.authorization?.split(" ")[1];
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader || !authorizationHeader.includes("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Authorization header missing or invalid",
+      });
+    }
+
+    const token = authorizationHeader.split("Bearer ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token not found in authorization header",
+      });
+    }
+
     const decoded = jwt.verify(token, process.env.jwt_secret);
+
     if (decoded.userId) {
       req.body.userIdFromToken = decoded.userId;
       next();
     } else {
-      return res.send({
+      return res.status(401).json({
         success: false,
         message: "Invalid token",
       });
     }
   } catch (error) {
-    return res.send({
+    return res.status(500).json({
       success: false,
       message: error.message,
     });
